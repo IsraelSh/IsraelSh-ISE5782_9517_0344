57c338de8f0678195dfe38668cfd227ce6e84e65
סנדי פרדס <sendi8475@gmail.com>
2022-03-20 20:21:11 +0200

    0.0.0.11*

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -26,7 +26,23 @@ public class Tube implements Geometry{
     }
     @Override
     public Vector getNormal(Point p){
-        return null;
+        //get ray point and vector
+        Point p0 = axisRay.getP0();
+        Vector v = axisRay.getDir();
+
+        //get point on the same level as the given point
+        double t = v.dotProduct(p.subtract(p0));//t=v(p-p0), t is the projection of the vector (p-p0) on the ray v
+
+        //if the point is not on the same level then get the point
+        //and return the normal
+        if(!Util.isZero(t))
+        {
+            Point o = p0.add(v.scale(t));//o=p0+t*v
+            return p.subtract(o).normalize();
+        }
+
+        //if the point is on the same level then return normal
+        return p.subtract(p0).normalize();
     }
     @Override
     public String toString() {
