11d241a4eab9bc6000aefc6e764d3b84dbb49ba6
Israel <israelshlomo855@gmail.com>
2022-03-21 17:21:02 +0200

    0.0.0.14

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -6,23 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
 
-    @Test
-    void getSpoint() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void testToString() {
-    }
 
     @Test
     void add() {
+        Point p1 = new Point(1,2,3);
+        Vector v = new Vector(5,6,8);
+        Point addingPiont = new Point(6 , 8 , 11);
+        assertEquals(p1.add(v) , addingPiont , "the add() function returns wrong value.");
+        //Boundary
+        assertThrows(IllegalArgumentException.class,() -> p1.subtract(p1) ,
+                "do not throw error when subtracts the same point");
     }
 
     @Test
     void subtract() {
+        Point p1 = new Point(1,2,3);
+        Point p2 = new Point(5,6,8);
+        Vector subtractionVector = new Vector(-4 , -4 , -5);
+        assertEquals(p1.subtract(p2) , subtractionVector , "the subtraction() function returns wrong value.");
+        assertThrows(IllegalArgumentException.class,() -> p1.subtract(p1) ,
+                "do not throw error when subtracts the same point");
     }
 }
