790e40907178c62394f17f5800ea4df1b7e71634
סנדי פרדס <sendi8475@gmail.com>
2022-04-04 23:24:47 +0300

    0.0.0.25

---
+++ src/renderer/Camera.java
@@ -0,0 +1,210 @@
+package renderer;
+
+import geometries.Intersectable;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static primitives.Util.alignZero;
+import static primitives.Util.isZero;
+
+public class Camera {
+    private Vector vTo;
+    private Vector vUp;
+    private Vector vRight;
+    private Point p0;
+    private int distance;
+    private double width;
+    private double height;
+
+    /**
+     * Constructor to create new Camera.
+     * @param p0 the center point
+     * @param vTo1 MUST be orthogonal to vectorUp
+     * @param vUp1 MUST be orthogonal to vectorTo
+     * @throws IllegalArgumentException if vectorTo isn't orthogonal to vectorUp
+     */
+    public Camera(Point p0, Vector vTo1, Vector vUp1) throws IllegalArgumentException {
+        this.p0 = p0;
+        this.vTo = vTo1.normalize();
+        this.vUp = vUp1.normalize();
+        if (!isZero(vTo1.dotProduct(vUp1))) {
+            throw new IllegalArgumentException("ERROR: vectorTo and vectorUp must be orthogonal");
+        }
+        this.vRight = vTo1.crossProduct(vUp1).normalize();
+    }
+
+    public Point getP0() {return p0;}
+
+    public Vector getvTo() {return vTo;}
+
+    public Vector getvUp() {return vUp;}
+
+    public Vector getvRight() {return vRight;}
+
+    public int getDistance() {return distance;}
+
+    /**
+     * Set the distance from the camera to the view plane.
+     * @param distance the distance from the camera to the view plane, MUST be not negative or
+     *     zero
+     * @return the camera itself
+     */
+    public Camera setVPDistance(int distance) {
+        this.distance = distance;
+        return this;
+    }
+
+    public double getWidth() {return width;}
+
+    public double getHeight() {return height;}
+
+    /**
+     * Set the width and the height of the view plane.
+     * @param width the width of the view plane, MUST be not negative or zero
+     * @param height the height of the view plane, MUST be not negative or zero
+     * @return the camera itself
+     */
+    public Camera setVPSize(double width, double height) {
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+
+    /**
+     * Helper for {@link #rotation(double, int)}.
+     * @param vector the vector to rotate
+     * @param r an angle in radians
+     * @return the result vector after the rotation
+     */
+    private Vector twistZ(Vector vector, double r) {
+        double cos = Math.cos(r);
+        double sin = Math.sin(r);
+        double axisX = vector.getX() * cos - vector.getY() * sin;
+        double axisY = vector.getX() * sin + vector.getY() * cos;
+        return new Vector(axisX, axisY, vector.getZ());
+    }
+
+    /**
+     * Rotating the camera by degree in radians.
+     * @param r the degrees in radians
+     * @param a to which axis to rotate
+     * @return the camera itself
+     */
+    public Camera rotation(double r, int a) {
+        if(a == 0) {
+            vTo = twistX(vTo, r);
+            vUp = twistX(vUp, r);
+        }
+        if(a == 1) {
+            vTo = twistY(vTo, r);
+            vUp = twistY(vUp, r);
+        }
+        if(a == 2) {
+            vTo = twistZ(vTo, r);
+            vUp = twistZ(vUp, r);
+        }
+        vRight = vTo.crossProduct(vUp);
+        return this;
+    }
+
+
+    /**
+     * Helper for {@link #rotation(double, int)}.
+     * @param vector the vector to rotate
+     * @param r an angle in radians
+     * @return the result vector after the rotation
+     */
+    private Vector twistY(Vector vector, double r) {
+        double cos = Math.cos(r);
+        double sin = Math.sin(r);
+        double axisX = vector.getX() * cos + vector.getZ() * sin;
+        double axisZ = vector.getZ() * cos - vector.getX() * sin;
+        return new Vector(axisX, vector.getY(), axisZ);
+    }
+
+    /**
+     * Helper for {@link #rotation(double, int)}.
+     * @param vector the vector to rotate
+     * @param r an angle in radians
+     * @return the result vector after the rotation
+     */
+    private Vector twistX(Vector vector, double r) {
+        double cos = Math.cos(r);
+        double sin = Math.sin(r);
+        double axisY = vector.getY() * cos - vector.getZ() * sin;
+        double axisZ = vector.getY() * sin + vector.getZ() * cos;
+        return new Vector(vector.getX(), axisY, axisZ);
+    }
+
+
+    /**
+     * Find all the intersection points with the plan view and geometry.
+     * @param nX row count pixels
+     * @param nY column count pixels
+     * @param intersect the intersect-able object with the rays from the view plane
+     * @return list of all the points that intersect, if there is no intersect point return null
+     */
+    public List<Point> findRay(int nX, int nY, Intersectable intersect) {
+        Ray ray;
+        List<Point> result = new LinkedList<>();
+        List<Point> intersectionPoints;
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                ray = constructRay(nX, nY, i, j);
+                intersectionPoints = intersect.findIntersections(ray);
+                if (intersectionPoints != null) {
+                    result.addAll(intersectionPoints);
+                }
+            }
+        }
+        return result.isEmpty() ? null : result;
+    }
+
+    /**
+     * Shift the camera to the direction of the vector with the distance.
+     * @param vector direction with distance
+     * @return the camera itself
+     */
+    public Camera shift(Vector vector) {
+        p0 = p0.add(vector);
+        return this;
+    }
+
+    /**
+     * Find a ray from p0 to the center of the pixel from the given resolution.
+     * @param nX the number of the rows
+     * @param nY the number of the columns
+     * @param j column
+     * @param i row
+     * @return ray from p0 the center to the center of the pixel in row i column j
+     */
+    public Ray constructRay(int nX, int nY, int j, int i) {
+        Vector dir;
+        Point pCenter, pCenterPixel;
+        double ratioY, ratioX, yI, xJ;
+
+        pCenter = p0.add(vTo.scale(distance));
+        ratioY = alignZero(height / nY);
+        ratioX = alignZero(width / nX);
+
+        pCenterPixel = pCenter;
+        yI = alignZero(-1 * (i - (nY - 1) / 2d) * ratioY);
+        xJ = alignZero((j - (nX - 1) / 2d) * ratioX);
+        if (!isZero(xJ)) {
+            pCenterPixel = pCenterPixel.add(vRight.scale(xJ));
+        }
+        if (!isZero(yI)) {
+            pCenterPixel = pCenterPixel.add(vUp.scale(yI));
+        }
+        dir = pCenterPixel.subtract(p0);
+
+        return new Ray(dir,p0);
+    }
+
+
+}
---
+++ unittests/IntegrationTests.java
@@ -0,0 +1,161 @@
+import geometries.Plane;
+import geometries.Sphare;
+import geometries.Triangle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
+import renderer.Camera;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+     * Integration test for camera and shapes.
+     */
+    public class IntegrationTests {
+
+        private static Vector vTo;
+        private static Vector vUp;
+        private static Camera camera;
+
+        @BeforeAll
+        public static void setupAll() {
+            Point pZero = new Point(0, 0, 0);
+            vTo = new Vector(0, 0, -1);
+            vUp = new Vector(0, 1, 0);
+            camera = new Camera(pZero, vTo, vUp);
+        }
+
+    @Test
+    public void cameraPlaneIntersections() {
+
+
+        /* TC01: plane with 9 points that intersect */
+
+        /* Variables */
+        Plane planeTestCase1 = new Plane( new Vector(0, 0, 1),new Point(0, 0, -3));
+        camera.setVPDistance(1).setVPSize(3, 3);
+        List<Point> result1 = camera.findRay(3, 3, planeTestCase1);
+
+        /* Asserts */
+        assertEquals(9, result1.size(), "ERROR: Except for 9 intersect points");
+
+        /* TC02: plane with 9 points that intersect */
+
+        /* Variables */
+        Plane planeTestCase2 = new Plane(new Vector(0, 0.5, -1),new Point(0, 0, -2));
+        camera.setVPDistance(1).setVPSize(3, 3);
+        List<Point> result2 = camera.findRay(3, 3, planeTestCase2);
+
+        /* Asserts */
+        assertEquals(9, result2.size(), "ERROR: Except for 9 intersect points");
+
+
+        /* TC03: plane with 6 points that intersect */
+
+        /* Variables */
+        Plane planeTestCase3 = new Plane( new Vector(0, 4, -1),new Point(0, 0, -2));
+        camera.setVPDistance(1).setVPSize(3, 3);
+        List<Point> result3 = camera.findRay(3, 3, planeTestCase3);
+
+        /* Asserts */
+        assertEquals(6, result3.size(), "ERROR: Except for 9 intersect points");
+    }
+
+    @Test
+    public void cameraTriangleIntersections() {
+
+        /* TC01: triangle with one point that intersect */
+
+        /* Variables */
+        Triangle triangleTestCase1 = new Triangle(new Point(0, 1, -2), new Point(1, -1, -2),
+                new Point(-1, -1, -2));
+        camera.setVPDistance(1).setVPSize(3, 3);
+        List<Point> result1 = camera.findRay(3, 3, triangleTestCase1);
+
+        /* Asserts */
+        assertEquals(1, result1.size(), "ERROR: Except for one intersect point");
+
+        /* TC02: triangle with 2 points that intersect */
+
+        /* Variables */
+        Triangle triangleTestCase2 = new Triangle(new Point(0, 20, -2), new Point(1, -1, -2),
+                new Point(-1, -1, -2));
+        camera.setVPDistance(1).setVPSize(3, 3);
+        List<Point> result2 = camera.findRay(3, 3, triangleTestCase2);
+
+        /* Asserts */
+        assertEquals(2, result2.size(), "ERROR: Except for 2 intersect points");
+    }
+        @Test
+        public void cameraSphereIntersections() {
+            /* TC01: sphere with 2 points that intersect */
+
+            /* Variables */
+            Sphare sphereTC01 = new Sphare(new Point(0, 0, -3), 1);
+            camera.setVPDistance(1).setVPSize(3, 3);
+            List<Point> result01 = camera.findRay(3, 3, sphereTC01);
+            List<Point> exceptedResult01 = Arrays.asList(new Point(0, 0, -2), new Point(0, 0, -4));
+
+            /* Asserts */
+            Assertions.assertNotNull(result01,
+                    "ERROR: There are no intersection, and except for 2 points");
+            assertTrue(
+                    exceptedResult01.size() == result01.size() && exceptedResult01.containsAll(result01),
+                    "ERROR: There aren't the same points that intersect");
+
+            /* TC02: sphere with 18 points that intersect */
+
+            /* Variables */
+            Sphare sphereTC02 = new Sphare(new Point(0, 0, -2.5), 2.5);
+            Camera cameraTC02 = new Camera(new Point(0, 0, 0.5), vTo, vUp);
+            cameraTC02.setVPDistance(1).setVPSize(3, 3);
+            List<Point> result02 = cameraTC02.findRay(3, 3, sphereTC02);
+
+            /* Asserts */
+            assertEquals(18, result02.size(), "ERROR: Except for 18 intersect points");
+
+            /*
+             * TC03: sphere with 10 points that intersect,
+             * part of the sphere is behind the view plane
+             */
+
+            /* Variables */
+            Sphare sphereTC03 = new Sphare(new Point(0, 0, -2), 2);
+            Camera cameraTC03 = new Camera(new Point(0, 0, 0.5), vTo, vUp);
+            cameraTC03.setVPDistance(1).setVPSize(3, 3);
+            List<Point> result03 = cameraTC03.findRay(3, 3, sphereTC03);
+
+            /* Asserts */
+            assertEquals(10, result03.size(), "ERROR: Except for 10 intersect points");
+
+
+            /* TC04: sphere with 9 points that intersect, part of the sphere is behind the camera */
+
+            /* Variables */
+            Sphare sphereTC04 = new Sphare(new Point(0, 0, 0), 4);
+            Camera cameraTC04 = new Camera(new Point(0, 0, 1), vTo, vUp);
+            cameraTC04.setVPDistance(1).setVPSize(3, 3);
+            List<Point> result04 = cameraTC04.findRay(3, 3, sphereTC04);
+
+            /* Asserts */
+            assertEquals(9, result04.size(), "ERROR: Except for 9 intersect points");
+
+            /* TC05: sphere with no intersect, the sphere is behind the camera */
+
+            /* Variables */
+            Sphare sphereTC05 = new Sphare(new Point(0, 0, 1), 0.5);
+            camera.setVPDistance(1).setVPSize(3, 3);
+            List<Point> result05 = camera.findRay(3, 3, sphereTC05);
+
+            /* Asserts */
+            assertNull(result05, "ERROR: Except for null, there are no intersect points");
+        }
+
+
+    }
+
